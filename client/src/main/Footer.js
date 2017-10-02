795f0773e9aede16dc932ed901e7f6cba823d507
lzibetti <lzibetti@comcast.net>
2017-10-02 11:40:55 -0600

    finished footer

--- client/src/main/Footer.js
+++ client/src/main/Footer.js
@@ -3,8 +3,9 @@ import React, { Component } from 'react';
 class Footer extends Component {
     render() {
         return (
-            <div>
-                <h6>AT FOOTER</h6>
+            <div className="footer">
+                <span>beardsAmerica@org.com &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    (800)321-9444 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Fillmore, UT &nbsp;&nbsp; &nbsp;84641</span>
             </div>
         );
     }
